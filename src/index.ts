3513448f7b654189e0fcfe671a7d28ae8c3694f1
Alex <yaroshenko.sashko@gmail.com>
2022-09-26 15:02:50 +0200

    fullParser is added(not ready)

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ bot.setMyCommands([
     {command: '/info', description: 'Terms of using'}
 ])
 bot.on("message", (msg: any) => {
-    const text: string = msg.text
+    let text: string = msg.text
     const chatId: number = msg.chat.id
     console.log(msg)
     if (text == '/start'){
@@ -17,5 +17,6 @@ bot.on("message", (msg: any) => {
     if (text == '/info'){
         return bot.sendMessage(chatId, "If you wanna use calculator just write a statment in format x + y (or x+y). \nAvalible operations:\n+\n-\n*\n/ (division with remainder) \n// (division without remainder)\n% (remainder of division\n** or ^ (power)\n√\nAlso sorry, but numbers higher then 2^53 (or 9007199254740992), woun't have anfractional part and if you calculating numbers smaller then 0.0000001 you may not get correct answer 😒.")
     }
+    text = text.split(' ').join('`')
     return bot.sendMessage(chatId, parser(text))
 })
